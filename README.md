ee7a01853915d6b7ca730bb8a3b6cfd864868c11
mblode <m.blode@gmail.com>
2019-01-24 21:07:25 +1100

    Change name to Zotero Citation Picker

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
   <img src="https://github.com/mblode/vscode-zotero/blob/master/images/icon.png?raw=true" alt="" width=100 height=100>
 </a>
 
-# VS Code Zotero
+# VS Code Zotero Citation Picker
 
 This package adds Zotero support to VS Code Markdown editing. To use it, you will need to have the Better BibTeX plugin installed in Zotero.
 
 ## Features
 
-If you don't feel like typing citations out (and let's be honest, you don't), executing 'Zotero Picker' will call up a graphical picker which will insert these for you, formatted and all.
+If you don't feel like typing citations out (and let's be honest, you don't), executing 'Zotero Citation Picker' will call up a graphical picker which will insert these for you, formatted and all.
 
-- Activate via Command Palette (command + shift + P): Type "Zotero Picker" and press enter.
+- Activate via Command Palette (command + shift + P): Type "Zotero Citation Picker" and press enter.
 - Activae via keyboard shortcut: Use alt+shift+z
 
 ![Screenshot](images/screenshot.png)
--- src/extension.js
+++ src/extension.js
@@ -35,16 +35,16 @@ const showZoteroPicker = () => {
 function activate(context) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "zotero" is now active!');
+	console.log('Congratulations, your extension "zotero citation picker" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.zotero', function() {
+	let disposable = vscode.commands.registerCommand('extension.zoteroCitationPicker', function() {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Show Zotero Picker');
+		vscode.window.showInformationMessage('Show Zotero Citation Picker');
 		showZoteroPicker();
 	});
 
