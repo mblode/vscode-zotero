5d161c96bb0d47b6785874c3970c4ebfffecb7b5
mblode <m.blode@gmail.com>
2019-01-25 13:03:39 +1100

    v0.1.2

--- src/extension.js
+++ src/extension.js
@@ -44,7 +44,6 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Show Zotero Citation Picker');
 		showZoteroPicker();
 	});
 
